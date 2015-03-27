6828ad49c2d723cce549d9f88aed50fdd4104d80
Mac <Mac@Macs-MacBook-Pro-2.local>
2015-03-26 21:22:41 -0500

    Initial commit

---
+++ src/main/java/com/here/demo/web/MainController.java
@@ -0,0 +1,25 @@
+package com.here.demo.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+/**
+ * Created by Mac on 3/26/15.
+ */
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/greeting")
+    public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+        model.addAttribute("name", name);
+        return "greeting";
+    }
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+}
